4ec2bc98e11adb0d09f4f76aa05bdfd1580da079
icarovie <icarovieirra@gmail.com>
2019-09-12 00:09:59 -0300

    Classes iniciais

---
+++ esboco.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <vector>
+
+class Grafo{
+    vector <Vertice> G;
+
+class Vertice{
+    int idade;
+    vector <string> comandados;
+    vector <string> comandantes;
+}
